d59de43d311632a1581e763c752080e03785506c
CGM123sherry <g.christinemurithi@gmail.com>
2024-09-07 18:43:33 +0300

    python-p3 fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,32 @@
-def write_file(file_name, file_content):
-    pass
 
-def append_file(file_name, append_content):
-    pass
+# lib/file_io.py
+def write_file(file_name, file_content):
+   
+    # my directory
+    full_file_name = f"{file_name}.txt"
+    
+    # Opening the file in write mode ('w')
+    with open(full_file_name, 'w') as file:
+        file.write(file_content)
+
+
+def append_file(file_name, file_content):
+    
+    # directory
+    full_file_name = f"{file_name}.txt"
+    
+    # Opening the file in append mode ('a')
+    with open(full_file_name, 'a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+ 
+    # directory
+    full_file_name = f"{file_name}.txt"
+    
+    # Open the file in read mode ('r')
+    try:
+        with open(full_file_name, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return f"Error: {file_name}.txt not found"
